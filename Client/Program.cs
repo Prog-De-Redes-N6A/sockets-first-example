be566035a57f7e2f6274f66af543a006a8309eaa
SabeMP <sabemasterprox@gmail.com>
2025-09-07 11:24:20 -0300

    Add Example

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Client Application..");
+
+            Socket clientSocket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+
+            IPEndPoint localEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+            clientSocket.Bind(localEndpoint);
+
+            IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            // We connect to the server
+            clientSocket.Connect(serverEndpoint); // Blocking
+            
+            Console.WriteLine("Connected to server!!");
+
+            Console.WriteLine("Type a message for the server:");
+            string message = Console.ReadLine();
+            byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+            clientSocket.Send(messageBytes);
+            Console.WriteLine("Sent message...");
+            Console.ReadLine();
+        }
+    }
+}
